d3bae636097c525d8897576f8e59eac68a0beda7
smosco <go1ruf2tk3@gmail.com>
2023-10-02 15:46:24 +0900

    feat: 공통 Layout 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/GlobalStyle';
 import GlobalFonts from './styles/GlobalFont';
+import Header from './components/Header';
 
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [],
+  },
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+]);
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/signUp',
-      element: <SignUp />,
-    },
-  ]);
   return (
     <>
       <GlobalStyle />
