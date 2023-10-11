cee3b2759ce7df9f407d66241099d1e933710532
hjy0951 <hjy0951@gmail.com>
2023-10-11 17:54:35 +0900

    feat: 할 일 추가시 라벨링 기능 구현

--- src/components/Modal/AddTask.tsx
+++ src/components/Modal/AddTask.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
+import { IoClose } from 'react-icons/io5';
 import { ModalButton } from './CommonModalStyles';
 import SelectBox from '../SelectBox';
 import { ReactComponent as DeadlineCheck } from '../../assets/images/deadlineCheck.svg';
@@ -29,6 +30,7 @@ const InputField = styled.div`
   }
 
   input {
+    height: 2.5rem;
     padding: 1rem;
     background-color: #fafafa;
     border-radius: 8px;
@@ -41,6 +43,7 @@ const InputField = styled.div`
 `;
 
 const Fields = styled.div`
+  height: 4.5rem;
   display: flex;
   gap: 3rem;
 `;
@@ -90,6 +93,64 @@ const DeadlineField = styled.div`
   }
 `;
 
+const LabelsContainer = styled.div`
+  height: 5rem;
+  padding: 0.3rem 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  overflow: auto;
+  align-items: center;
+  gap: 0.5rem;
+  background-color: #fafafa;
+  border-radius: 8px;
+  font-size: 0.9rem;
+
+  ul {
+    padding: 1rem 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    overflow: auto;
+    gap: 0.5rem;
+  }
+
+  #task-label-input {
+    width: 5rem;
+    height: 2rem;
+  }
+`;
+
+const LabelItem = styled.li`
+  position: relative;
+  padding: 0.5rem 1rem;
+  height: 2rem;
+  border-radius: 10px;
+  color: black;
+  text-align: center;
+  line-height: 100%;
+  background-color: skyblue;
+
+  button {
+    display: none;
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+    bottom: 1rem;
+    right: -0.3rem;
+    color: #f44336;
+    background-color: white;
+    border: 1px solid lightgray;
+    border-radius: 50%;
+    line-height: 110%;
+  }
+
+  &:hover button {
+    display: flex;
+  }
+`;
+
 interface Props {
   members: string[][];
 }
@@ -97,18 +158,43 @@ interface Props {
 export default function AddTaskModal({ members }: Props) {
   const today = new Date();
 
+  const tagInput = useRef<HTMLInputElement>(null);
   const [taskName, setTaskName] = useState<string>('');
   const [assignee, setAssignee] = useState<string>('');
   const [checkDeadline, setCheckDeadline] = useState<boolean>(false);
   const [deadline, setDeadline] = useState<string>(
     [today.getFullYear(), today.getMonth() + 1, today.getDate()].join('-'),
   );
+  const [tags, setTags] = useState<string[]>(['label1', 'label2']);
 
   const options = members.map((member) => {
     return { value: member[1], label: member[0] };
   });
 
   console.log(assignee);
+
+  const enterTag = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (tagInput.current === null) return;
+    if (e.key === 'Enter') {
+      // TODO: alert decoration
+      if (tagInput.current.value.length === 0) {
+        alert('최소 1글자 이상 입력해주세요!');
+        return;
+      }
+      if (tags.includes(tagInput.current.value)) {
+        alert('이미 등록된 라벨입니다!');
+        return;
+      }
+      setTags([...tags, tagInput.current.value]);
+      tagInput.current.value = '';
+    }
+  };
+
+  const onClickDeleteTag = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.currentTarget.id.split('-')[1];
+    const updateTags = tags.filter((tag) => tag !== target);
+    setTags(updateTags);
+  };
 
   return (
     <Wrapper>
@@ -151,7 +237,24 @@ export default function AddTaskModal({ members }: Props) {
             )}
           </DeadlineField>
         </Fields>
-
+        <InputField>
+          <label htmlFor="task-label-input">
+            레이블
+            <LabelsContainer>
+              <ul id="tags">
+                {tags.map((tag) => (
+                  <LabelItem key={tag}>
+                    <span>{tag}</span>
+                    <button id={`delete-${tag}`} type="button" onClick={onClickDeleteTag}>
+                      <IoClose />
+                    </button>
+                  </LabelItem>
+                ))}
+                <input id="task-label-input" type="text" onKeyUp={enterTag} ref={tagInput} />
+              </ul>
+            </LabelsContainer>
+          </label>
+        </InputField>
         <ModalButton type="button">추가하기</ModalButton>
       </FormContainer>
     </Wrapper>
