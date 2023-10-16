7c0108181f9115859bfc0f540375cd67af98fabd
hjy0951 <hjy0951@gmail.com>
2023-10-16 23:12:10 +0900

    fix: header 프로필 사진이 기본 이미지가 아닌 다른 이미지로 적용되는 버그 수정

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 
-import defaultProfileImg from '@assets/images/boardIllust.svg';
+import defaultProfileImg from '@assets/images/defaultProfileImg.svg';
 
 type DropdownOption = {
   id: number;
