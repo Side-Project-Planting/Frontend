19c36c65b6069b99f260cf6b0c71f13230122da7
hjy0951 <hjy0951@gmail.com>
2023-10-19 14:55:47 +0900

    chore: 누락된 path alias 추가 적용

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -2,8 +2,8 @@ import React, { useState, useRef } from 'react';
 
 import styled from 'styled-components';
 
-import Dropdown from './Dropdown';
-import TaskItem from './TaskItem';
+import Dropdown from '@components/Dropdown';
+import TaskItem from '@components/TaskItem';
 
 interface Label {
   id: number;
