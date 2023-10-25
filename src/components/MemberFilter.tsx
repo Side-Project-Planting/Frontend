c02d837811c11bea38e4b1b86b1afb216b35453f
smosco <go1ruf2tk3@gmail.com>
2023-10-25 16:34:36 +0900

    feat: 멤버 필터링 이미지 hover시 이름 보여주는 툴팁 구현

--- src/components/MemberFilter.tsx
+++ src/components/MemberFilter.tsx
@@ -15,26 +15,45 @@ interface IMmeberFilterProp {
 
 const Memberlist = styled.ul`
   width: fit-content;
+  height: 3rem;
   display: flex;
   align-items: center;
   gap: 0.8rem;
 `;
 
 const MemberItem = styled.li`
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    .tooltip {
+      display: block;
+    }
+  }
+`;
+
+const MemberImg = styled.img`
   width: 3rem;
   height: 3rem;
   border-radius: 50%;
-  overflow: hidden;
-  cursor: pointer;
-
   &.selected {
     border: 3px solid #64d4ab;
   }
 `;
 
-const MemberImg = styled.img`
-  width: 100%;
-  height: 100%;
+const Tooltip = styled.span`
+  display: none;
+  position: absolute;
+  bottom: -1.2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 3.2rem;
+  padding: 2px 5px;
+  text-align: center;
+  border-radius: 5px;
+  font-size: 0.8em;
+  color: #fff;
+  background: #494b5a;
 `;
 
 export default function MemberFilter({ selectedMember, onClick }: IMmeberFilterProp) {
@@ -44,12 +63,9 @@ export default function MemberFilter({ selectedMember, onClick }: IMmeberFilterP
   return (
     <Memberlist>
       {modifiedMemberList.map((item) => (
-        <MemberItem
-          key={item.id}
-          onClick={() => onClick(item.id)}
-          className={`${selectedMember === item.id && 'selected'}`}
-        >
-          <MemberImg src={item.imgUrl} alt="profile-img" />
+        <MemberItem key={item.id} onClick={() => onClick(item.id)}>
+          <MemberImg src={item.imgUrl} alt="profile-img" className={`${selectedMember === item.id && 'selected'}`} />
+          {item.id !== 0 && <Tooltip className="tooltip">{item.name}</Tooltip>}
         </MemberItem>
       ))}
     </Memberlist>
