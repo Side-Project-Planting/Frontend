7cc119e5217df48290ffe8724bed1ceeefcdcded
smosco <go1ruf2tk3@gmail.com>
2024-03-03 19:49:55 +0900

    design: 로그인 페이지 스타일 수정

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export const MainWrapper = styled.div`
-  min-height: 100dvh;
-  padding-block: 3rem;
+  min-height: 100vh;
+  padding: 3rem;
   display: flex;
-  gap: 4rem;
+  gap: 2rem;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -13,13 +13,13 @@ export const MainWrapper = styled.div`
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 2.5rem;
+  gap: 2rem;
   justify-content: center;
   align-items: center;
 `;
 
 export const Descripton = styled.div`
-  font-size: 2rem;
+  font-size: 1.5rem;
 `;
 
 export const HighlightSpan = styled.span`
@@ -31,7 +31,7 @@ export const GoogleOAuthButton = styled.button`
   height: 3.5rem;
   background-color: #ffffff;
   border: 1px solid #dde2e8;
-  border-radius: 8px;
+  border-radius: 1rem;
 `;
 
 export const ButtonContainer = styled.div`
@@ -39,13 +39,14 @@ export const ButtonContainer = styled.div`
   gap: 1rem;
   align-items: center;
   justify-content: center;
-  font-weight: 700;
+  font-family: 'Pretendard-Bold';
 `;
 
 export const LoginPolicy = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.8rem;
+  gap: 0.5rem;
+  font-size: 0.8rem;
   color: #8993a1;
 `;
